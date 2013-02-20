3439ca80466a5123bedab222c9c64513e37603fa
Ilya Shpigor <petrsum@gmail.com>
2013-02-20 14:11:18 +0400

    Implement printing of the port's value

--- Makefile
+++ Makefile
@@ -3,10 +3,10 @@ SOURCE=main.c
 #CFLAGS=-D__KERNEL__ -I/usr/src/linux-headers-3.2.0-38/include
 #LDFLAGS=
 
-all: $(BIN)
+all: clean $(BIN)
 
 $(BIN): $(SOURCE)
 	gcc $(CFLAGS) -o $(BIN) $(SOURCE)
 
 clean:
-	rm *.o $(BIN)
+	rm -f *.o $(BIN)
--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <unistd.h>
 #include <sys/io.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
 
 static const unsigned long kBaseAddress = 0x330;
 static const unsigned long kRegistersCount = 3;
@@ -10,10 +12,15 @@ void OpenPort()
     int rc;
     rc = ioperm(kBaseAddress, kRegistersCount, 1);
     if ( rc != 0 )
+    {
+        printf("OpenPort() - error = %d\n", errno);
         exit(0);
+    }
 }
 
 int main(int argc, char **argv)
 {
     OpenPort();
+
+    printf("0x330 = 0x%x\n", inb(kBaseAddress));
 }
